849159f50cf96f6669bf14afd79effb25a23a794
bdfbol <ben.dye@fbol.com>
2022-09-30 10:00:41 -0500

    Completed Studio 4.8

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,12 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+
+            Student Ben = new Student("Ben", 1, 1, 4.0);
+
+            Console.WriteLine("The Student class works! " + Ben.Name + " is a student!");
+
+            Console.WriteLine(Course);
         }
     }
 }
---
+++ SchoolPractice/Teacher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    class Teacher
+    {
+        public Teacher
+        {
+            private static int nextTeacherID = 1;
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int TeacherID { get; set; }
+            public string Subject { get; set; }
+        public int YearsTeaching { get; set; } = 0;
+
+        public Teacher(string firstName, string lastName, int teacherID, string subject, int yearsTeaching)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            TeacherID = teacherID;
+            Subject = subject;
+            YearsTeaching = yearsTeaching;
+        }
+
+        public Teacher(string firstName, string lastName, int teacherID, string subject): this(firstName, lastName, teacherID, subject, 0) { }
+
+        public Teacher(string firstName): this(firstName, nextTeacherID)
+        {
+            nextTeacherID++;
+        }
+        }
+    }
+}
+
